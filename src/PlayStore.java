09d1f2389260485be6844024845de7b5182b75e5
BenCole6 <bencole@y7mail.com>
2018-05-10 16:14:06 +1000

    update

--- src/PlayStore.java
+++ src/PlayStore.java
@@ -11,58 +11,61 @@ import java.util.*;
 
 
 abstract class Content{
-String ID;
-String applicationName;
-int numberOfdowns;
-double appPrice;
- ArrayList<Comment> review = new ArrayList<Comment>();
-     public Content(String id, String Cname, int dwonloads, double CappPrice ,ArrayList<Comment> Creview ) 
-			{
-			ID = id;
-			applicationName = Cname;
-			numberOfdowns =  dwonloads;
-			appPrice = CappPrice;
-			review = Creview;
-			
-			}	
-     public Content(String id, String Cname, int dwonloads, double CappPrice  ) 
-		{
+	String ID;
+	String applicationName;
+	int numberOfdowns;
+	double appPrice;
+	ArrayList<Comment> review = new ArrayList<Comment>();
+	ArrayList<Content> Contents = new ArrayList<Content>();
+
+	public Content(String id, String Cname, int dwonloads, double CappPrice ,ArrayList<Comment> Creview ) 
+	{
 		ID = id;
 		applicationName = Cname;
 		numberOfdowns =  dwonloads;
 		appPrice = CappPrice;
+		review = Creview;
+
+	}	
 	
-		}	
-  
-      /*
-       * constructor without  ArrayList<Comment>
-       *  public Content(String id, String name, int dwonloads, double CappPrice  ) 
+	public Content(String id, String Cname, int dwonloads, double CappPrice  ) 
+	{
+		ID = id;
+		applicationName = Cname;
+		numberOfdowns =  dwonloads;
+		appPrice = CappPrice;
+
+	}	
+
+	/*
+	 * constructor without  ArrayList<Comment>
+	 *  public Content(String id, String name, int dwonloads, double CappPrice  ) 
 			{
 			ID = id;
 			applicationName = name;
 			numberOfdowns =  dwonloads;
 			price = CappPrice;
-			
+
 			}*/	
-     public Content(String id, String cname,  double CappPrice  ) 
-		{
+	public Content(String id, String cname,  double CappPrice  ) 
+	{
 		ID = id;
 		applicationName = cname;
 		numberOfdowns =  0;
 		appPrice = CappPrice;
-		
-		}	
-     
-     public Content(String id, String cname  ) 
-		{
+
+	}	
+
+	public Content(String id, String cname  ) 
+	{
 		ID = id;
 		applicationName = cname;
 		numberOfdowns =  0;
 		appPrice = 0;// the price 
-		
-		}
-     
-    
+
+	}
+
+
 }
 /////////////////////////////////////////////////////////////
 class Game extends Content {
@@ -73,97 +76,102 @@ class Game extends Content {
 		isMultiPlayer = isMultiPl;
 		os = cos;
 	}
-	
+
 	public Game(String id, String cname,  double CappPrice,  boolean isMultiPl ,OS cos ) {
 		super(id, cname,CappPrice);
 		isMultiPlayer = isMultiPl;
 		os = cos;
 		//Game g1 = new Game (â€œG101â€�, â€œPokemonâ€�, 10, false, os1);
 	}
-	
+
 	public Game(String id, String name,  boolean isMultiPl ,OS cos) {
 		super(id, name);
 		isMultiPlayer = isMultiPl;
 		os = cos;
 		// Game g1 = new Game (â€œG101â€�, â€œPokemonâ€�, false, os1);
 	}
-	
-	
-	
+
+
+
 }
 ////////////////////////////////////////////////////////////////////////////////////////
 abstract class Reading extends Content {
 	String publisher;
 	String genre;
 	int  numberOfpages;
-	
-	public Reading (String id, String name, int dwonloads, double AppPrice, String Cpublisher,String Cgenre, int CnumberOfpages ) {
+
+	public Reading (String id, String name, int dwonloads, double AppPrice, String Cpublisher,String Cgenre, int CnumberOfpages )
+	{
 		super(id, name,dwonloads,AppPrice);
 		publisher = Cpublisher;
 		genre = Cgenre; 
 		numberOfpages = CnumberOfpages;
-			
+
 	}
-	
-	public Reading (String id, String name, double AppPrice, String Cpublisher,String Cgenre, int CnumberOfpages ) {
+
+	public Reading (String id, String name, double AppPrice, String Cpublisher,String Cgenre, int CnumberOfpages )
+	{
 		super(id, name,AppPrice);
 		publisher = Cpublisher;
 		genre = Cgenre; 
 		numberOfpages = CnumberOfpages;
-			
+
 	}
-	public Reading (String id, String name, String Cpublisher,String Cgenre, int CnumberOfpages ) {
+	
+	public Reading (String id, String name, String Cpublisher,String Cgenre, int CnumberOfpages )
+	{
 		super(id, name);
 		publisher = Cpublisher;
 		genre = Cgenre; 
 		numberOfpages = CnumberOfpages;
-			
+
 	}
+
 }
 ////////////////////////////////////////////////////////////////////////////////////
 class Book extends Reading {
 	String[] author;
-public 	Book(String id, String name, int dwonloads, double AppPrice, String Cpublisher,String Cgenre, int numberOfpages, String[] Cauthor) {
-	super ( id,  name,  dwonloads,  AppPrice,  Cpublisher, Cgenre,  numberOfpages);
-	author = Cauthor;
-	
-}
-	
-public 	Book(String id, String name, double AppPrice, String Cpublisher,String Cgenre, int numberOfpages, String[] Cauthor) {
-	super ( id,  name,   AppPrice,  Cpublisher, Cgenre,  numberOfpages);
-	author = Cauthor;
-	
+	public 	Book(String id, String name, int dwonloads, double AppPrice, String Cpublisher,String Cgenre, int numberOfpages, String[] Cauthor) {
+		super ( id,  name,  dwonloads,  AppPrice,  Cpublisher, Cgenre,  numberOfpages);
+		author = Cauthor;
+
+	}
+
+	public 	Book(String id, String name, double AppPrice, String Cpublisher,String Cgenre, int numberOfpages, String[] Cauthor) {
+		super ( id,  name,   AppPrice,  Cpublisher, Cgenre,  numberOfpages);
+		author = Cauthor;
+
+	}	
+
+	public 	Book(String id, String name,  String Cpublisher,String Cgenre, int numberOfpages, String[] Cauthor) {
+		super ( id,  name,  Cpublisher, Cgenre,  numberOfpages);
+		author = Cauthor;
+
+	}
+
 }	
 
-public 	Book(String id, String name,  String Cpublisher,String Cgenre, int numberOfpages, String[] Cauthor) {
-	super ( id,  name,  Cpublisher, Cgenre,  numberOfpages);
-	author = Cauthor;
-	
-}
-	
-}	
-	
 /////////////////////////////////////////////////////////////////////////////////
 class Magazine extends Reading {
 	String title;
-public 	Magazine(String id, String name, int dwonloads, double AppPrice, String Cpublisher,String Cgenre, int numberOfpages, String Ctitle) {
-	super ( id,  name,  dwonloads,  AppPrice,  Cpublisher, Cgenre,  numberOfpages);
-	title = Ctitle;
-	
-}
-	
-public 	Magazine(String id, String name, double AppPrice, String Cpublisher,String Cgenre, int numberOfpages, String Ctitle) {
-	super ( id,  name,   AppPrice,  Cpublisher, Cgenre,  numberOfpages);
-	title = Ctitle;
-	//Magazine(â€œM21â€�, â€œForbesâ€�, 8, â€œForbes Mediaâ€�, â€œBusinessâ€�, 50,â€œWorldâ€™s richest under 30â€�);
-}	
+	public 	Magazine(String id, String name, int dwonloads, double AppPrice, String Cpublisher,String Cgenre, int numberOfpages, String Ctitle) {
+		super ( id,  name,  dwonloads,  AppPrice,  Cpublisher, Cgenre,  numberOfpages);
+		title = Ctitle;
 
-public 	Magazine(String id, String name,  String Cpublisher,String Cgenre, int numberOfpages, String Ctitle) {
-	super ( id,  name,  Cpublisher, Cgenre,  numberOfpages);
-	title = Ctitle;
-	//Magazine(â€œM21â€�, â€œForbesâ€�,  â€œForbes Mediaâ€�, â€œBusinessâ€�, 50,â€œWorldâ€™s richest under 30â€�);	
-}
-	
+	}
+
+	public 	Magazine(String id, String name, double AppPrice, String Cpublisher,String Cgenre, int numberOfpages, String Ctitle) {
+		super ( id,  name,   AppPrice,  Cpublisher, Cgenre,  numberOfpages);
+		title = Ctitle;
+		//Magazine(â€œM21â€�, â€œForbesâ€�, 8, â€œForbes Mediaâ€�, â€œBusinessâ€�, 50,â€œWorldâ€™s richest under 30â€�);
+	}	
+
+	public 	Magazine(String id, String name,  String Cpublisher,String Cgenre, int numberOfpages, String Ctitle) {
+		super ( id,  name,  Cpublisher, Cgenre,  numberOfpages);
+		title = Ctitle;
+		//Magazine(â€œM21â€�, â€œForbesâ€�,  â€œForbes Mediaâ€�, â€œBusinessâ€�, 50,â€œWorldâ€™s richest under 30â€�);	
+	}
+
 }	
 
 /////////////////////////////////////////////////////////////////////////////////
@@ -173,98 +181,99 @@ class Comment{
 	String comment;
 	Object [] commentArray;
 	Comment(User u, String comm){
-	user = u;
-	comment = comm;
-	//Comment cmnt = new Comment(u1, â€œThis is a fantastic game!â€�);
+		user = u;
+		comment = comm;
+		//Comment cmnt = new Comment(u1, â€œThis is a fantastic game!â€�);
 	}
-	
+
 }
 /////////////////////////////////////////////////////////////////////////////////
 class OS{
-	
-String TypeOfOS;
-int versionNumber;
 
-public OS(String OStype, int versionNum) {
-	TypeOfOS = 	OStype;
-	versionNumber = versionNum;
-	
-	//OS os1 = new OS(â€œAndroidâ€�, 4);
-	
-}
-	
+	String TypeOfOS;
+	int versionNumber;
+
+	public OS(String OStype, int versionNum) {
+		TypeOfOS = 	OStype;
+		versionNumber = versionNum;
+
+		//OS os1 = new OS(â€œAndroidâ€�, 4);
+
+	}
+
 }
 /////////////////////////////////////////////////////////////////////////////////
 class User{
-	
-String ID;
-String name;
-String phonNumber;
-double balance;
-private OS os;
-private Content content;
-ArrayList<String> premium = new ArrayList<String>();
-public User (String id, String Cname, String CphoneNum, double Cbalance, OS Cos) {
-	
-ID = id;
-name = Cname;
-phonNumber = CphoneNum;
-balance = Cbalance;
-os = Cos;
-}
 
-public User (String id, String Cname, String CphoneNum, OS Cos) {
-	
-ID = id;
-name = Cname;
-phonNumber = CphoneNum;
-os = Cos;
+	String ID;
+	String name;
+	String phonNumber;
+	double balance;
+	private OS os;
+	private Content content;
+	ArrayList<String> premium = new ArrayList<String>();
+	public User (String id, String Cname, String CphoneNum, double Cbalance, OS Cos) {
 
-}
-public void becomePremium(User usr) {
-	// misunderstanding 
-	if ( balance < 0 )
-	{
-		System.out.println("insufficient balance.");
+		ID = id;
+		name = Cname;
+		phonNumber = CphoneNum;
+		balance = Cbalance;
+		os = Cos;
 	}
-	else if (content.appPrice > 100)
-	{
-		
-		premium.add(name);
-		
-	}
-}
-public String getuser() {
-return 	name;
-}
 
-public void buyContent(Content con)
-  {
-	
+	public User (String id, String Cname, String CphoneNum, OS Cos) {
+
+		ID = id;
+		name = Cname;
+		phonNumber = CphoneNum;
+		os = Cos;
+
+	}
+	public void becomePremium(User usr) {
+		// misunderstanding 
+		if ( balance < 0 )
+		{
+			System.out.println("insufficient balance.");
+		}
+		else if (content.appPrice > 100)
+		{
+
+			premium.add(name);
+
+		}
+	}
+	public String getuser() {
+		return 	name;
+	}
+
+	public void buyContent(Content con)
+	{
+
 		balance -= content.appPrice;
 		for (String var : premium) 
 		{ 
-		 if(var == name)   
-		 {
-			content.appPrice=content.appPrice *(0.8); 
-		 }
+			if(var == name)   
+			{
+				content.appPrice=content.appPrice *(0.8); 
+			}
 		}
-       content.numberOfdowns +=1;
-       
-       if (con instanceof Game )   
-       {
-    	   
-    	   
-       }
-  }
+		content.numberOfdowns +=1;
+
+		if (con instanceof Game )   
+		{
+
+
+		}
+	}
 
 }
-/////////////////////////////////////////////////////////////////////////////////
+
+
 public class PlayStore {
 
 	public static void main(String[] args) {
 
-	/*
+		/*
 PlayStore admin = new PlayStore();
 		//adding new readings
 		String[] authors ={"L. Tolstoy"};
@@ -344,4 +353,12 @@ PlayStore admin = new PlayStore();
 		g1.printAllReview(); */
 	}
 
+	public genrePull()
+	{
+		System.println("Please type the genre of book/magazine that you would like to retrieve");
+		genreSelect = new userIn.next();
+		System.out.println();
+
+		List<Contents>
+	}
 }
